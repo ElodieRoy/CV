abc863e14d6c0f804b245735ba601bd61c249bf3
Elodie <roy.elodie2@gmail.com>
2021-07-16 09:08:04 +0200

    [sections] init

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { makeStyles, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core"
 
 import DesktopMenu from "./menus/DesktopMenu"
 import MobileMenu from "./menus/MobileMenu"
