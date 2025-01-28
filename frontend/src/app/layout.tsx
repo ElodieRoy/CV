24bb495869237b5bf01cd66c699d5e2a8ab32461
elroy <roy.elodie2@gmail.com>
2025-01-28 10:15:52 +0100

    feat: enhance global styles and layout for better responsiveness

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className="container mx-auto flex min-h-screen flex-col border font-sans">
+      <body className="m-4 mx-auto size-[calc(100%-2rem)] border font-sans lg:my-8 lg:h-[calc(100%-4rem)] lg:max-w-5xl lg:border-red-600">
         <Header />
         {children}
       </body>
