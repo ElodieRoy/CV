10dba92443440bb4a089283dbca2a360e553ed8f
elroy <roy.elodie2@gmail.com>
2025-01-31 12:06:22 +0100

    feat: add hover animated link

--- frontend/src/components/navbar/NavBarContent.tsx
+++ frontend/src/components/navbar/NavBarContent.tsx
@@ -11,11 +11,11 @@ export function NavBarContent({ onItemClick }: NavBarContentProps) {
   return (
     <ul className="flex flex-col lg:flex-row gap-10">
       {menu.map(([key, value]) => (
-        <li key={key}>
-          <a className="relative" href={`#${key}`} onClick={onItemClick}>
+        <li key={key} className="relative group w-fit" onClick={onItemClick}>
+          <a className="relative" href={`#${key}`}>
             {value}
           </a>
-          <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
+          <span className="absolute left-0 -bottom-1 w-0 h-1 lg:h-0.5 rounded-md bg-light transition-all duration-300 group-hover:w-full" />
         </li>
       ))}
     </ul>
