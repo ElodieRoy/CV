4f8fa82c47faf9e92e7a7cb08f7d04b96c2a8f64
elroy <roy.elodie2@gmail.com>
2025-01-31 11:58:53 +0100

    feat: init i18n translation

--- frontend/src/components/navbar/NavBarContent.tsx
+++ frontend/src/components/navbar/NavBarContent.tsx
@@ -1,22 +1,21 @@
-const menu = [
-  { link: "#profile", label: "Profil" },
-  { link: "#skills", label: "Compétences" },
-  { link: "#experiences", label: "Expériences" },
-  { link: "#contact", label: "Contact" },
-];
+import { useTranslation } from "react-i18next";
 
 type NavBarContentProps = {
   onItemClick?: () => void;
 };
 
 export function NavBarContent({ onItemClick }: NavBarContentProps) {
+  const { t } = useTranslation();
+  const menu = Object.entries(t("menu", { returnObjects: true }));
+
   return (
     <ul className="flex flex-col lg:flex-row gap-10">
-      {menu.map((item) => (
-        <li key={item.label}>
-          <a href={item.link} onClick={onItemClick}>
-            {item.label}
+      {menu.map(([key, value]) => (
+        <li key={key}>
+          <a className="relative" href={`#${key}`} onClick={onItemClick}>
+            {value}
           </a>
+          <span className="absolute left-0 bottom-0 w-0 h-0.5 bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
         </li>
       ))}
     </ul>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
+import "@/i18n/config.ts";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 );
