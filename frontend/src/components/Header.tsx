918ed320c714d3478ed94611b4302575ae43e812
elroy <roy.elodie2@gmail.com>
2025-01-31 18:17:40 +0100

    feat: modify bg

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { MobileNavBar } from "@/components/navbar/MobileNavBar";
 
 export function Header() {
   return (
-    <header className="fixed left-0 right-0 text-light p-4 lg:container mx-auto flex justify-end">
+    <header className="fixed left-0 right-0 text-light p-4 lg:container mx-auto flex justify-end z-1">
       <DesktopNavBar />
       <MobileNavBar />
     </header>
--- frontend/src/sections/Home.tsx
+++ frontend/src/sections/Home.tsx
@@ -1,11 +1,12 @@
 export function Home() {
   return (
     <section
-      className="bg-gradient-to-l from-pink-500/70 via-red-500/70 to-amber-500/70"
+      className="bg-linear-to-r/shorter lg:bg-linear-to-r/hsl from-red-500/70 lg:from-pink-500/70 lg:via-red-500/70 to-amber-500/70 text-light h-screen"
       id="home"
     >
-      {/*  */}
-      <div className="bg-[url('src/assets/bg2.webp')] bg-cover bg-center opacity-10 h-screen"></div>
+      <div className="bg-[url('src/assets/bg1.webp')] bg-cover bg-center opacity-10 h-full" />
+      <h1>Elodie ROY</h1>
+      <h2>Développeuse Full-Stack Web</h2>
     </section>
   );
 }
