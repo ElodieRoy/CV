2976c2f4832941044b493c2f3f516a5a336db00c
elroy <roy.elodie2@gmail.com>
2025-02-06 11:49:36 +0100

    feat: add more skills svg

--- frontend/src/sections/Skills.tsx
+++ frontend/src/sections/Skills.tsx
@@ -15,33 +15,46 @@ const techStack = [
       { name: "Redux", logo: "/icons/redux-original.svg" },
       { name: "TailwindCss", logo: "/icons/tailwindcss-original.svg" },
       { name: "CSS", logo: "/icons/css3-original.svg" },
+      { name: "Material UI", logo: "/icons/materialui-original.svg" },
+      { name: "Shadcn UI", logo: "/icons/shadcnui.svg" },
     ],
   },
   {
     category: "Backend",
     tools: [
       { name: "JAVA", logo: "/icons/java-original.svg" },
+      { name: "Spring", logo: "/icons/spring-original.svg" },
       { name: "Hibernate", logo: "/icons/hibernate-original.svg" },
       { name: "Node.js", logo: "/icons/nodejs-original.svg" },
       { name: "Express", logo: "/icons/express-original.svg" },
       { name: "tRPC", logo: "/icons/trpc-original.svg" },
-    ],
-  },
-  {
-    category: "Base de données",
-    tools: [
+      { name: "REST", logo: "/icons/apirest.svg" },
       { name: "PostgreSQL", logo: "/icons/postgresql-original.svg" },
       { name: "MySQL", logo: "/icons/mysql-original.svg" },
       { name: "ElasticSearch", logo: "/icons/elasticsearch-original.svg" },
     ],
   },
   {
-    category: "Autres Outils",
+    category: "Devops & CI",
     tools: [
       { name: "Git", logo: "/icons/git-original.svg" },
       { name: "Docker", logo: "/icons/docker-original.svg" },
       { name: "GitLab CI", logo: "/icons/gitlab-original.svg" },
+      { name: "GitHub", logo: "/icons/github-original.svg" },
+      { name: "PlayWright", logo: "/icons/playwright-original.svg" },
+      { name: "Jest", logo: "/icons/jest-original.svg" },
+    ],
+  },
+  {
+    category: "Autres outils",
+    tools: [
+      { name: "VS Code", logo: "/icons/vscode-original.svg" },
+      { name: "Linux", logo: "/icons/linux-original.svg" },
       { name: "Jira", logo: "/icons/jira-original.svg" },
+      { name: "SAP", logo: "/icons/sap.svg" },
+      { name: "Kibana", logo: "/icons/kibana-original.svg" },
+      { name: "Notion", logo: "/icons/notion-original.svg" },
+      { name: "XD", logo: "/icons/xd-original.svg" },
     ],
   },
 ];
