c2c79595de47d63aae37392dde40ffe8581e2691
elroy <roy.elodie2@gmail.com>
2025-02-08 12:55:56 +0100

    feat: add confetti effect on form submission

--- frontend/src/sections/Contact.tsx
+++ frontend/src/sections/Contact.tsx
@@ -1,30 +1,64 @@
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 import { Typography } from "@/components/ui/Typography";
+import confetti from "canvas-confetti";
+
+async function test(data: FormData) {
+  await new Promise((r) => setTimeout(r, 1000));
+  return console.log("🌸 ", data);
+}
 
 export function Contact() {
+  const onSubmit = async (data: FormData) => {
+    try {
+      console.log("🌸");
+
+      await test(data);
+      await confetti({
+        particleCount: 100,
+        spread: 70,
+        colors: [
+          "#F44336",
+          "#E91E63",
+          "#FFC107",
+          "#2196F3",
+          "#4CAF50",
+          "#9C27B0",
+          "#009688",
+        ],
+        origin: { y: 0.8 },
+      });
+    } catch {
+      console.error("🌸 ");
+    }
+  };
+
   return (
     <section id="contact">
       <Typography type="h3">Contactez-moi</Typography>
-      <form className="flex flex-wrap gap-4">
+      <form
+        className="flex flex-wrap gap-2 lg:gap-4 max-w-3xl mx-auto"
+        action={onSubmit}
+      >
         <Input
-          className="min-w-sm flex-1"
+          className="flex-1"
           type="text"
-          id="name"
           placeholder="Prénom/Nom"
+          id="name"
+          name="name"
         />
         <Input
-          className="min-w-sm flex-2"
+          className="flex-2"
           type="email"
-          id="email"
           placeholder="Email"
+          name="email"
         />
         <Input
           as="textarea"
           className="w-full"
           placeholder="Message"
-          id="w3review"
-          rows={6}
+          rows={8}
+          name="message"
         />
         <Button type="submit" className="w-full">
           Envoyer
