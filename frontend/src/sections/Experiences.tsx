5390bbf4abc43e3e349bdfca92aa1814dba0353a
elroy <roy.elodie2@gmail.com>
2025-02-26 14:40:40 +0100

    feat(frontend): enhance progress bar animation

--- frontend/src/sections/Experiences.tsx
+++ frontend/src/sections/Experiences.tsx
@@ -9,12 +9,12 @@ export function Experiences() {
 
     if (!imgGroup || !imgGroupContainer) return;
 
-    const items = imgGroup.getElementsByTagName("li");
+    const itemsLength = imgGroup.getElementsByTagName("li").length;
 
     // Animate horizontally during vertical scroll
     scroll(
       animate(imgGroup, {
-        transform: ["none", `translateX(-${items.length - 1}00vw)`],
+        transform: ["none", `translateX(-${itemsLength - 1}00vw)`],
       }),
       { target: imgGroupContainer }
     );
@@ -23,41 +23,56 @@ export function Experiences() {
     scroll(animate("#progress", { x: ["-100%", "0%"] }), {
       target: imgGroupContainer,
     });
-    scroll(animate("#progress-bar-end", { opacity: [0, 0, 0, 0, 1] }), {
-      target: imgGroupContainer,
-    });
+    scroll(
+      animate("#progress-bar-end", {
+        opacity: Array.from({ length: itemsLength }, (_, i) =>
+          i === itemsLength - 1 ? 1 : 0
+        ),
+      }),
+      {
+        target: imgGroupContainer,
+      }
+    );
   }, []);
 
   return (
     <section id="experiences">
       <Typography type="h3">Mon parcours</Typography>
-      <div id="img-group-container" className="h-[500vh] relative">
-        <div className="sticky top-0 overflow-hidden h-screen">
-          <ul id="img-group" className="flex">
+      <div id="img-group-container" className="h-[calc(500vh_+_80px)] relative">
+        <div className="sticky top-20 overflow-hidden h-[calc(100vh_-_56px)]">
+          <ul id="img-group" className="flex h-[calc(100%_-_80px)]">
             {["1", "2", "3", "4", "5"].map((i) => (
               <li
                 key={i}
-                className="flex w-screen h-screen items-center justify-center flex-col flex-none img-container"
+                className="flex w-screen h-full items-center justify-center flex-col flex-none img-container"
               >
                 <img
-                  className="w-80 h-96"
+                  className="w-80 h-1/2"
                   src={`https://examples.motion.dev/photos/cityscape/${i}.jpg`}
                 />
                 <h3>#00{i}</h3>
               </li>
             ))}
           </ul>
-          <div id="progress-bar" className="relative -top-16 h-1 bg-foreground">
-            <span
-              id="progress-bar-start"
-              className="absolute -top-1 rounded-full bg-primary size-3 z-1"
-            />
-            <span className="absolute -top-1 right-0 rounded-full bg-foreground size-3 z-1" />
-            <span
-              id="progress-bar-end"
-              className="absolute -top-1 right-0 rounded-full bg-primary size-3 z-2"
-            />
-            <div id="progress" className="h-full bg-primary" />
+          <div
+            id="progress-bar-container"
+            className="flex flex-col justify-center h-80px"
+          >
+            <div
+              id="progress-bar"
+              className="relative mt-auto h-1 bg-foreground"
+            >
+              <span
+                id="progress-bar-start"
+                className="absolute -top-1 rounded-full bg-primary size-3 z-1"
+              />
+              <span className="absolute -top-1 right-0 rounded-full bg-foreground size-3 z-1" />
+              <span
+                id="progress-bar-end"
+                className="absolute -top-1 right-0 rounded-full bg-primary size-3 z-2"
+              />
+              <div id="progress" className="h-full bg-primary" />
+            </div>
           </div>
         </div>
       </div>
