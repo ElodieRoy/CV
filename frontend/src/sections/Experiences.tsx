abed1a1e06284ef2564c6fa7a842304953abe2ce
elroy <roy.elodie2@gmail.com>
2025-02-28 14:26:39 +0100

    feat(frontend): improve role display

--- frontend/src/sections/Experiences.tsx
+++ frontend/src/sections/Experiences.tsx
@@ -31,47 +31,51 @@ export function Slider() {
       {experiences.map((experience) => (
         <li
           key={experience.id}
-          className="p-2 flex w-full h-full items-center flex-col flex-none img-container gap-4"
+          className="p-2 flex w-full h-full items-center flex-col flex-none img-container gap-4 lg:px-40"
         >
           <img src={experience.image} alt={experience.id} className="h-20" />
           <Typography type="h4">{experience.title}</Typography>
           <div className="flex h-10 justify-center items-center gap-4">
             {experience.logos.map((logo) => (
-              <div className="h-full w-20 bg-light py-1 px-2 rounded-lg shadow-md flex items-center justify-center">
-                <img
-                  key={logo.title}
-                  src={logo.url}
-                  alt={logo.title}
-                  className="max-w-full"
-                />
+              <div
+                key={logo.title}
+                className="h-full w-20 bg-light py-1 px-2 rounded-lg shadow-md flex items-center justify-center"
+              >
+                <img src={logo.url} alt={logo.title} className="max-w-full" />
               </div>
             ))}
           </div>
-          {experience.type === "education" && <p>{experience.description}</p>}
-          {experience.type === "job" && (
-            <div className="flex flex-col gap-4">
-              <div>
-                <Typography type="lead">Secteur:</Typography>
-                <span>{experience.secteur}</span>
-              </div>
+          <div className="px-6">
+            {experience.type === "education" && <p>{experience.description}</p>}
+            {experience.type === "job" && (
+              <div className="flex flex-col gap-4">
+                <div>
+                  <Typography type="lead">Secteurs d'activité:</Typography>
+                  <span>{experience.secteur}</span>
+                </div>
 
-              <div>
-                <Typography type="lead">Rôles:</Typography>
-                <ul className="list-disc list-inside ">
-                  {experience.roles.map((role, index) => (
-                    <li key={index} className="ml-4">
-                      {role}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+                <div>
+                  <Typography type="lead">Rôles:</Typography>
 
-              <div>
-                <Typography type="lead">Compétences:</Typography>
-                <span>{experience.skills}</span>
+                  <div className="list-disc list-inside">
+                    {experience.roles.map((role, index) => (
+                      <span
+                        key={index}
+                        className="block ml-2 lg:ml-4 before:content-['•'] before:mr-2"
+                      >
+                        {role}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+
+                <div>
+                  <Typography type="lead">Compétences:</Typography>
+                  <span>{experience.skills}</span>
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </li>
       ))}
     </ul>
