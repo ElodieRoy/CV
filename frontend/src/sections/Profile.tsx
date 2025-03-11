41dccd1ae69c9ae95b634d195e2031b117da7a1a
elroy <roy.elodie2@gmail.com>
2025-03-11 16:52:56 +0100

    fix(frontend): rerender app

--- frontend/src/sections/Profile.tsx
+++ frontend/src/sections/Profile.tsx
@@ -2,17 +2,19 @@ import { Section } from "@/components/Section";
 import { Typography } from "@/components/ui/Typography";
 import { CURRENT_YEAR } from "@/constants";
 import { ComponentPropsWithoutRef } from "react";
-import { Trans } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 
 export function Profile({
   className,
   id,
 }: ComponentPropsWithoutRef<"section">) {
   const nbExp = CURRENT_YEAR - 2021;
+  // use hook to rerender <Trans> component when language changes
+  const { t } = useTranslation();
   return (
     <Section id={id} className={className}>
       <Typography type="p" className="lg:text-xl lg:leading-relaxed">
-        <Trans i18nKey="about" ns="home" values={{ nbExp }} />
+        <Trans>{t("about", { nbExp, ns: "home" })}</Trans>
       </Typography>
       <Typography type="p" className="lg:text-xl lg:leading-relaxed">
         <Trans i18nKey="description" ns="home" />
